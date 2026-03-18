a72759ee4467311be812209b1496e39b30b5a699
zhaobingkun <zhaobingkun@139.com>
2026-03-18 21:17:42 +0800

    Revamp homepage visuals and bilingual layout polish

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,4 +11,26 @@
       nav.classList.toggle('open');
     });
   }
+
+  var revealNodes = document.querySelectorAll('[data-reveal]');
+  if (revealNodes.length) {
+    if ('IntersectionObserver' in window) {
+      var observer = new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('is-visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      }, { threshold: 0.15, rootMargin: '0px 0px -30px 0px' });
+
+      revealNodes.forEach(function (node) {
+        observer.observe(node);
+      });
+    } else {
+      revealNodes.forEach(function (node) {
+        node.classList.add('is-visible');
+      });
+    }
+  }
 })();
